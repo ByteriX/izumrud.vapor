f5bce808c97e5d06b7c5acbb3947ff80903aec31
Sergey Balalaev <sof.bix@mail.ru>
2023-08-01 23:32:37 +0400

    tests push

--- IzumrudTests/IzumrudTests.swift
+++ IzumrudTests/IzumrudTests.swift
@@ -8,10 +8,11 @@
 
 import XCTest
 import iSamaraCounters
+import iSamaraCountersModels
 
 final class IzumrudTests: XCTestCase {
 
-    let service = SamaraEnergoSendDataService()
+    let service = SamaraEnergoSendDataService(data: SamaraEnergoData(domain: "http://127.0.0.1:8080"))
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -25,12 +26,35 @@ final class IzumrudTests: XCTestCase {
         let expectation = expectation(description: "SamaraEnergo")
 
         service
-            .map(SendDataMockInput())
+            .map(SendDataMockInput(email: "sof.bix@mail.ru", electricCounterNumber: "123", dayElectricCount: "12", nightElectricCount: "8"))
             .done{ _ in
                 expectation.fulfill()
             }.catch{ error in
                 XCTFail(error.localizedDescription)
                 expectation.fulfill()
+            }
+
+        waitForExpectations(timeout: 30) { error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+            }
+        }
+    }
+
+    func testErrorSamaraEnergo() throws {
+        let expectation = expectation(description: "SamaraEnergo")
+
+        service
+            .map(SendDataMockInput(email: "!", electricCounterNumber: "123"))
+            .done{ _ in
+                XCTFail()
+                expectation.fulfill()
+            }.catch{ error in
+                defer {
+                    expectation.fulfill()
+                }
+                let error = error as NSError
+                XCTAssertEqual(417, error.code)
             }
 
         waitForExpectations(timeout: 30) { error in
